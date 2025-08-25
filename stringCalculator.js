270b7474551144f1185e3d9f467923b41ecee721
LBouzac <bouzacludovic@gmail.com>
2025-08-25 10:31:26 +0200

    ex 3

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,7 +3,11 @@ export function add(numbers) {
     if (numbers === '0' || numbers === '') {
         return 0;
     }
+
+    numbers = numbers.replace('\\n', ',')
+
     numbers = numbers.split(',').map(Number).reduce((a, b) => a + b, 0);
+    console.log(numbers);
 
     return numbers;
 }
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,7 +7,11 @@ describe('test of function add',() => {
         expect(add('0')).toBe(0)
     })
 
-    it('return infinity numbers', () => {
+    it('returns infinity numbers', () => {
         expect(add('1,2,3,4,5,6,7,8,9,10')).toBe(55)
+    })
+
+    it('returns numbers with \\n and , ', () => {
+        expect(add('1\\n2,3')).toBe(6)
     })
 })
